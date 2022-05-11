f61097786f3e8fa94a5ae789ef6da0e8f3037943
PaulBurak <ipaulburak@gmail.com>
2022-05-11 15:15:00 +0300

    First Commit

---
+++ HomeWork/HomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by Друг Сердечный on 11.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //var x=1
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
